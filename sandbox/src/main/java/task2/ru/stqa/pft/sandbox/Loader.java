209fa0e4157df09f75bd8bb111b61d2694bfabc5
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-03-15 20:52:38 +0200

    Создан класс Loader, который запускает программу вычисления расстояния от точки до точки.

---
+++ sandbox/src/main/java/task2/ru/stqa/pft/sandbox/Loader.java
@@ -0,0 +1,10 @@
+package task2.ru.stqa.pft.sandbox;
+
+public class Loader {
+  public static void main (String[] args) {
+    Point p1 = new Point(8, 5);
+    Point p2 = new Point(3, 1);
+
+    System.out.println("Hello. The distance between two dots  is " + p1.distance(p2) + " centimeters!");
+  }
+}
