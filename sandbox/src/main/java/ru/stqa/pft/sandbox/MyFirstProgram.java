bedd76207d36bf8f9072d74c74545236ea0229bd
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-03-23 15:47:55 +0300

    Прошла миграция на другой компьютер

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,7 +7,7 @@ public class MyFirstProgram {
         Square s = new Square(5);
         System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
         Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+        System.out.println("Площадь прямоугольника со стороной " + r.a + " ? " + r.b + " = " + r.area());
         }
         public static void hello(String somebody){
           System.out.println("Hello, " + somebody + "!");
