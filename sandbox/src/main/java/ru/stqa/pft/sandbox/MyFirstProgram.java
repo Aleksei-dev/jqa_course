940a92c27cce2df54c295467646a98e7dbe96001
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-03-27 09:56:57 +0200

    Создан тест вычисления дискриминанта

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,15 +1,15 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args){
-        hello("Aleksei");
+  public static void main(String[] args){
+    hello("Aleksei");
 
-        Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
-        Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со стороной " + r.a + " ? " + r.b + " = " + r.area());
-        }
-        public static void hello(String somebody){
-          System.out.println("Hello, " + somebody + "!");
-        }
+      Square s = new Square(5);
+      System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+      Rectangle r = new Rectangle(4, 6);
+      System.out.println("Площадь прямоугольника со стороной " + r.a + " ? " + r.b + " = " + r.area());
+    }
+  public static void hello(String somebody){
+    System.out.println("Hello, " + somebody + "!");
+  }
 }
