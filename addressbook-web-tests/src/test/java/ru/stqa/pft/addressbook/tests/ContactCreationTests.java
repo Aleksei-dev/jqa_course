2279a99cec06a75484d9fa675a40678220230b2e
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-04-06 08:26:36 +0300

    Добавлена лямбда функция вычисления максималького идентификатора
    Добавил лямбда выражение вычисления максимального идентификатора в тесте создания Контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,18 +12,12 @@ public class ContactCreationTests extends TestBase{
   @Test
   public void testContactCreation() throws Exception {
     List<ContactData> before = app.getContactHelper().getContactList();
-    ContactData contact = new ContactData("Aleksei", "Kovaltsuk", "Tallinn", "+37250000005", "mail@test.com", "test1");
+    ContactData contact = new ContactData("Aleksey", "Kovaltsuk", "Tallinn", "+37250000005", "mail@test.com", "test1");
     app.getContactHelper().createContact(contact, true);
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max){
-        max = c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     app.logout();
