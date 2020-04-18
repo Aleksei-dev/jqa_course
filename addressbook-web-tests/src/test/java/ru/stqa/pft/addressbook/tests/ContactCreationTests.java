c905572a8aac3e45ff47b04d6660b01cf28d52ec
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-04-18 16:53:48 +0300

    Отделили тесты от тестовых данных при тесте создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -18,11 +18,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
+  public Iterator<Object[]> validContacts() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{new ContactData().withFirstname("Alek").withLastname("Baldin").withAddress("Earth 1")});
-    list.add(new Object[]{new ContactData().withFirstname("Alek2").withLastname("Baldin2").withAddress("Earth 2")});
-    list.add(new Object[]{new ContactData().withFirstname("Alek3").withLastname("Baldin3").withAddress("Earth 3")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[]{new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
@@ -34,7 +38,7 @@ public class ContactCreationTests extends TestBase {
     }
   }
 
-  @Test(dataProvider = "validContacts")
+  @Test (dataProvider = "validContacts")
   public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
