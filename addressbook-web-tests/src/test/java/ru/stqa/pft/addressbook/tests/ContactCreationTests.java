41f3671a26a7d92430101cabaa2006b9ef81e3ee
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-04-18 16:42:39 +0300

    Параметризовал тестовые данные для теста создания контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,35 +1,43 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-public class ContactCreationTests extends TestBase{
+public class ContactCreationTests extends TestBase {
+
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[]{new ContactData().withFirstname("Alek").withLastname("Baldin").withAddress("Earth 1")});
+    list.add(new Object[]{new ContactData().withFirstname("Alek2").withLastname("Baldin2").withAddress("Earth 2")});
+    list.add(new Object[]{new ContactData().withFirstname("Alek3").withLastname("Baldin3").withAddress("Earth 3")});
+    return list.iterator();
+  }
 
   @BeforeMethod
-  public void ensurePreconditions(){
+  public void ensurePreconditions() {
     app.goTo().groupPage();
-    if(app.group().all().size() == 0){
+    if (app.group().all().size() == 0) {
       app.group().create((new GroupData().withName("test2").withHeader("test3").withFooter("test4")));
     }
   }
 
-  @Test
-  public void testContactCreation() {
+  @Test(dataProvider = "validContacts")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    File photo = new File("src/test/resources/Jellyfish.jpg");
-    ContactData contact = new ContactData()
-            .withFirstname("Abc")
-            .withLastname("Def")
-            .withAddress("Moon 2");
     app.contact().create(contact, true);
     assertThat(app.contact().getContactCount(), equalTo(before.size() + 1));
     Contacts after = app.contact().all();
@@ -37,7 +45,7 @@ public class ContactCreationTests extends TestBase{
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
 
-  @Test (enabled = false)
+  @Test(enabled = false)
   public void testBadContactCreation() {
     app.goTo().homePage();
     Contacts before = app.contact().all();
