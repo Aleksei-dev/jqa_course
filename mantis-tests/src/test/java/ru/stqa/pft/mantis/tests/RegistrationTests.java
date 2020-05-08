177351cd745835cb31f527d957eb16316970a9db
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-05-08 07:50:29 +0300

    Добавлена возможность использовать почтовый сервис Telnet используя James

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -23,10 +23,12 @@ public class RegistrationTests extends TestBase{
   public void testRegistration() throws IOException, MessagingException {
     long now = System.currentTimeMillis();
     String email = String.format("user%s@localhost.localdomain", now);
-    String user = String.format("user", now);
+    String user = String.format("user%s", now);
     String password = "password";
+    //app.james().createUser(user, password);
     app.registration().start(user, email);
     List<MailMessage> mailMessages = app.mail().waitForMail(2, 10000);
+    //List<MailMessage> mailMessages = app.james().waitForMail(user, password, 60000);
     String confirmationLink = findConfirmationLink(mailMessages, email);
     app.registration().finish(confirmationLink, password);
     assertTrue(app.newSession().login(user, password));
