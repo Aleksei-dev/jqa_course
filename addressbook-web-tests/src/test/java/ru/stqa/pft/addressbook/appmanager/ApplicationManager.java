4dcc5aca321bad65bf8c54386409d798aba7c903
Aleksei-dev <62004244+Aleksei-dev@users.noreply.github.com>
2020-05-15 13:03:37 +0300

    Jenkins jobs

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -51,7 +51,7 @@ public class ApplicationManager {
     } else {
       DesiredCapabilities capabilities = new DesiredCapabilities();
       capabilities.setBrowserName(browser);
-      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7")));
+      capabilities.setPlatform(Platform.fromString(System.getProperty("platform", "win7"))); //set WINDOWS in Jenkins
       wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
     }
     wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
